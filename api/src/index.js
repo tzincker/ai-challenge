18bf877c6388f5c5e670aa494f442b9ece8409f2
Tomas Zincker <tomas.zincker@perficient.com>
2025-07-25 15:14:51 -0400

    feat: add basic login API endpoint that returns JWT token

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//process.loadEnvFile();
+
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
@@ -13,8 +15,11 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
 
   const username = req.body.username;
+  const user = { username };
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
-  res.send('Hello World!');
+  res.json({ accessToken });
 });
 
 app.get('/chat', (req, res) => {
